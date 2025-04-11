013f13e381314024ba60f6467859625637d71752
Jan Paul Batrina <jpmbatrina01@gmail.com>
2025-04-11 09:21:34 +0800

    chore: Only re-add dummy values when ddl-auto is not empty

--- src/main/java/com/jbatrina/EmployeeManagementSystem/EmployeeManagementSystemApplication.java
+++ src/main/java/com/jbatrina/EmployeeManagementSystem/EmployeeManagementSystemApplication.java
@@ -1,6 +1,7 @@
 package com.jbatrina.EmployeeManagementSystem;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -18,6 +19,9 @@ public class EmployeeManagementSystemApplication implements CommandLineRunner {
 	@Autowired
 	RoleService roleService;
 
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String ddlAuto;
+
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeeManagementSystemApplication.class, args);
 	}
@@ -25,28 +29,32 @@ public class EmployeeManagementSystemApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		// NOTE: database is re-created each run (via ddl-auto)
-		// We add dummy values to populate database
-		// add roles and users
 
-		// TODO: add roles and users to permanent storage
-		Role adminRole = roleService.addRole(new Role("ADMIN"));
-		Role userRole = roleService.addRole(new Role("USER"));
+		System.out.println("DDL AUTO" + ddlAuto);
+		if (ddlAuto != null && !ddlAuto.isBlank()) {
+			// We add dummy values to populate database
+			// add roles and users
 
-		userService.addAdminUser(new User(
-				"admin",
-				"admin@admin.com",
-				"user",	// password provided as separate arg
-				Set.of(adminRole)),
-				"admin"
-		);
+			// TODO: add roles and users to permanent storage
+			Role adminRole = roleService.addRole(new Role("ADMIN"));
+			Role userRole = roleService.addRole(new Role("USER"));
+			
+			// add users
+			userService.addAdminUser(new User(
+					"admin",
+					"admin@admin.com",
+					"user",	// password provided as separate arg
+					Set.of(adminRole)),
+					"admin"
+			);
 
-		userService.addNormalUser(new User(
-				"user",
-				"user@user.com",
-				"user",	// password provided as separate arg
-				Set.of(userRole)),
-				"user"
-		);
+			userService.addNormalUser(new User(
+					"user",
+					"user@user.com",
+					"user",	// password provided as separate arg
+					Set.of(userRole)),
+					"user"
+			);
+		}
 	}
-
 }
